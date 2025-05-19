af3be45534b6c6af8af6db9edf4eb26225c65638
sabrinaabade7 <sabrina.abade@escolar.ifrn.edu.br>
2025-05-19 11:57:10 -0300

    19/05

--- projeto1/projeto1/Assets/Play.cs
+++ projeto1/projeto1/Assets/Play.cs
@@ -1,29 +1,80 @@
 using UnityEngine;
-
-public class Player : MonoBehaviour
+public class TesteSoma : MonoBehaviour
 {
-    private int numero;
-    public float velocidade = 40;
+    private int A;
+    private int B;
+    private int C;
+    
+    int Soma(int a, int b)
+    {
+        return a + b;
+    }
+    
+    int Soma2(int a, int b)
+    {
+        int c = a + b;
+        return c;
+    }
+
+    void MairQue(int a, int b)
+    {
+        if (a > b)
+        {
+            Debug.Log("Avarievel A armazena: " 
+                      + a + " que é maior que "+ b+".");   
+        }
+        else if (a < b)
+        {
+            Debug.Log("Avarievel A armazena: " 
+                      + a + " que é menor que "+ b+".");  
+        }
+        else
+        {
+            Debug.Log("Avarievel A armazena: " 
+                      + a + " que é igual que "+ b+".");  
+        }
+ 
+    }
 
     void Start()
     {
-        numero = 0;
-    }
+        A = 23;
+        B = 12; 
+        C = A + B;
+        //Debug.Log(C);
+        
+        //Debug.Log(Soma(13,67));
+        //Debug.Log(Soma(A,B));
+        //Debug.Log(Soma(89,C));
+        
+        /*
+        Debug.Log(Soma2(13,67));
+        Debug.Log(Soma2(A,B));
+        Debug.Log(Soma2(89,C));
+        */
 
+        /*
+        if (A > B)
+        {
+            Debug.Log("Avarievel A armazena: "
+                      + A + " que é maior que "+ B+".");
+        }
+        else
+        {
+            Debug.Log("Avarievel A armazena: "
+                      + A + " que é menor que "+ B+".");
+        }
+        */
+
+        MairQue(A,B);
+        MairQue(23,3);
+        MairQue(1,3);
+        MairQue(1,1);
+
+
+    }
     void Update()
     {
-        //Debug.log(numero);
-        //numerp = numero + 1;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            gameObject.transform.position += new Vector3(velocidade, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-
-        }
+        
     }
-
 }
