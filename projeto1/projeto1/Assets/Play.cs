c10cfd464be279d8d404ff0abfea49dd60e6896b
sabrinaabade7 <sabrina.abade@escolar.ifrn.edu.br>
2025-06-24 08:35:47 -0300

    2406

--- projeto1/projeto1/Assets/Play.cs
+++ projeto1/projeto1/Assets/Play.cs
@@ -1,80 +1,41 @@
 using UnityEngine;
-public class TesteSoma : MonoBehaviour
-{
-    private int A;
-    private int B;
-    private int C;
-    
-    int Soma(int a, int b)
-    {
-        return a + b;
-    }
-    
-    int Soma2(int a, int b)
-    {
-        int c = a + b;
-        return c;
-    }
 
-    void MairQue(int a, int b)
-    {
-        if (a > b)
-        {
-            Debug.Log("Avarievel A armazena: " 
-                      + a + " que é maior que "+ b+".");   
-        }
-        else if (a < b)
-        {
-            Debug.Log("Avarievel A armazena: " 
-                      + a + " que é menor que "+ b+".");  
-        }
-        else
-        {
-            Debug.Log("Avarievel A armazena: " 
-                      + a + " que é igual que "+ b+".");  
-        }
- 
-    }
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpHeight = 10f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float checkRadius = 0.2f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
 
     void Start()
     {
-        A = 23;
-        B = 12; 
-        C = A + B;
-        //Debug.Log(C);
-        
-        //Debug.Log(Soma(13,67));
-        //Debug.Log(Soma(A,B));
-        //Debug.Log(Soma(89,C));
-        
-        /*
-        Debug.Log(Soma2(13,67));
-        Debug.Log(Soma2(A,B));
-        Debug.Log(Soma2(89,C));
-        */
-
-        /*
-        if (A > B)
-        {
-            Debug.Log("Avarievel A armazena: "
-                      + A + " que é maior que "+ B+".");
-        }
-        else
-        {
-            Debug.Log("Avarievel A armazena: "
-                      + A + " que é menor que "+ B+".");
-        }
-        */
-
-        MairQue(A,B);
-        MairQue(23,3);
-        MairQue(1,3);
-        MairQue(1,1);
-
-
+        rb = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
-        
+        // Movimento horizontal
+        float inputX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
+
+        // Verificar se está tocando o chão
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        // Pulo
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Visualização da área de verificação do chão no editor
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
     }
 }
